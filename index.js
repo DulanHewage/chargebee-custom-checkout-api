447039aae14e5d6689cc3d93f7e33f5bd777118c
Dulan666 <dulanhewage2@gmail.com>
2022-10-14 00:47:16 +0530

    add new API endpoint (/api/customers) update package.json

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const port = 3000
 app.use(cors())
 app.use(express.urlencoded({extended:false}))
 
+chargebee.configure(
+  {
+    site : "poliigon-test",
+    api_key : "test_hJdA7C4QBzAdoAjzCpw0ZI6lo7ONkCaA"
+  }
+)
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.post('/api/generate_payment_intent', (req, res) => {
-
-  chargebee.configure(
-    {
-      site : "poliigon-test",
-      api_key : "test_hJdA7C4QBzAdoAjzCpw0ZI6lo7ONkCaA"
-    }
-  )
 
   chargebee.payment_intent.create(req.body).request(function(error,result) {
     if(error){
@@ -28,6 +28,20 @@ app.post('/api/generate_payment_intent', (req, res) => {
       const payment_intent = result.payment_intent
       res.json(payment_intent)
     }
+  })
+
+})
+
+app.get('/api/customers', (req, res) => {
+  chargebee.customer.list().request(function(error,result) {
+
+    if(error){
+      //handle error
+      res.json(error)
+    } else {
+      res.json(result.list)
+    }  
+
   })
 
 })
