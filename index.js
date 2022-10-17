b849056076f5c63f9a56a99d1c4e05c418366592
Dulan666 <dulanhewage2@gmail.com>
2022-10-18 01:21:45 +0530

    Add .env file and move API keys from the index.js add new APIs - (item_price/create, /item/create, /item_family/list, item/list) install new npm package "dotenv"

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const cors = require('cors')
 const chargebee = require("chargebee")
 const app = express()
 const port = 3000
+require('dotenv').config()
 
 app.use(cors())
 app.use(express.urlencoded({extended:false}))
+app.use(express.json());
 
 chargebee.configure(
   {
-    site : "poliigon-test",
-    api_key : "test_hJdA7C4QBzAdoAjzCpw0ZI6lo7ONkCaA"
+    site : process.env.CHARGEBEE_PUBLISHABLE_SITE,
+    api_key : process.env.CHARGEBEE_KEY
   }
 )
 
@@ -18,19 +20,19 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// create payment intent
 app.post('/api/generate_payment_intent', (req, res) => {
   chargebee.payment_intent.create(req.body).request(function(error,result) {
     if(error){
       //handle error
       res.json(error)
     }else{
-      const payment_intent = result.payment_intent
-      res.json(payment_intent)
+      res.json(result.payment_intent)
     }
   })
-
 })
 
+// Customer CRUD operations
 app.get('/api/customer/list', (req, res) => {
   chargebee.customer.list().request(function(error,result) {
 
@@ -67,7 +69,7 @@ app.put('/api/customer', (req, res) => {
     }
   })
 })
-// Delete a customer
+
 app.delete('/api/customer', (req, res) => {
   chargebee.customer.delete(req.body.customer_id).request(function(error,result) {
     if(error){
@@ -90,17 +92,19 @@ app.get('/api/customer', (req, res) => {
   })
 })
 
+// Create invoice
 app.post('/api/create_invoice', (req, res) => {
   chargebee.invoice.create_for_charge_items_and_charges(req.body).request(function(error,result) {
     if(error){
       //handle error
       res.json(error)
     }else{
-      res.json(result.invoice)
+      res.json(result)
     }
   })
 })
 
+// get item price list
 app.get('/api/item_price/list', (req, res) => {
   chargebee.item_price.list().request(function(error,result) {
     if(error){
@@ -110,6 +114,69 @@ app.get('/api/item_price/list', (req, res) => {
       res.json(result.list)
       }
     })
+})
+
+// create new item price
+app.post('/api/item_price/create', (req, res) => {
+  chargebee.item_price.create({
+    id : "hundred-credits",
+    item_id : "gold-credits",
+    name : "hundred credits",
+    pricing_model : "flat_fee",
+    price : 100,
+    external_name : "100 credits",
+    currency_code: "USD",
+    item_type: "charge"
+  }).request(function(error,result) {
+    if(error){
+      //handle error
+      res.json(error);
+    }else{
+      res.json(result);
+    }
+  })
+})
+
+// create new item
+app.post('/api/item/create', (req, res) => {
+  chargebee.item.create({
+    id : "gold-credits",
+    name : "Gold Credits",
+    status: "active",
+    item_family_id: "direct-purchase",
+    type: "charge",
+  }).request(function(error,result) {
+    if(error){
+      //handle error
+      res.json(error);
+    }else{
+      res.json(result);
+    }
+  })
+})
+
+//get item familiy list
+app.get('/api/item_family/list', (req, res) => {
+  chargebee.item_family.list().request(function(error,result) {
+    if(error){
+      //handle error
+      res.json(error);
+    }else{
+      res.json(result);
+      }
+    });
+})
+
+// get item list
+app.get('/api/item/list', (req, res) => {
+  chargebee.item.list().request(function(error,result) {
+    if(error){
+      //handle error
+      res,json(error);
+    }else{
+      res.json(result);
+      }
+    });
 })
 
 app.listen(port, () => {
